3754c27dd995cf4ddf479d443d07201198282b50
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-05 22:02:59 -0600

    put first boolean example in .js file and changed .js link in HTML file.

--- project2.js
+++ project2.js
@@ -1,1 +1,19 @@
 alert("JavaScript works!!!");
+
+// Activity: Boolean Logic
+//var p = true, q = false, r = false;
+// Given: (p and q) or r
+// if ((p && q) || r) {
+//     console.log("The expression is true");
+//  } else {
+//     console.log("The expression is false");
+// }
+
+var p = true, q = false, r = false;
+
+// for p and (q or r), example..
+if ((p && q) || r) {
+    console.log("The expression is true");
+} else {
+    console.log("The expression is false");
+}
