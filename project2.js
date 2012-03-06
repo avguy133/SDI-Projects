bf250dc81a3d1751a165e28951617fc41935995f
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-05 22:18:39 -0600

    updated the console.log outputs to make logical statements.

--- project2.js
+++ project2.js
@@ -11,23 +11,24 @@ alert("JavaScript works!!!");
 
 var p = true, q = false, r = false;
 
+console.log("Given p = true, q = false, r = false,");
 // for p and (q or r), example..
 if ((p && q) || r) {
-    console.log("The expression is true");
+    console.log("The expression 'p and (q or r)' is true");
 } else {
-    console.log("The expression is false");
+    console.log("The expression 'p and (q or r)' is false");
 }
 
 // for (p and not q) or (q and r), example..
 if ((p && !q) || (q && r)) {
-    console.log("The expression is true");
+    console.log("The expression '(p and not q) or (q and r)' is true");
 } else {
-    console.log("The expression is false");
+    console.log("The expression '(p and not q) or (q and r)' is false");
 }
 
 // for not(p or r), example..
 if (!(p || r)) {
-    console.log("The expression is true");
+    console.log("The expression 'not(p or r)' is true");
 } else {
-    console.log("The expression is false");
+    console.log("The expression 'not(p or r)' is false");
 }
