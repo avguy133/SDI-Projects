c6b5a795e63bea34028eb4a8320442fc3cb55be1
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-07 11:39:34 -0600

    added some array data to the .js and corrected the link to the .html file. also added a 'while' loop and a 'for' loop.

--- westad_zach_assignment2.js
+++ westad_zach_assignment2.js
@@ -1,4 +1,90 @@
-alert("JavaScript works!!!");
+// Author: Zach Westad
+// Created On: 03-1-2012
+// Last Edit: 03-7-2012
+// Deliverable 2
+// Custom Car Audio
+
+// function to shorten 'console.log()' statements wit say().
+var say = function(message) { console.log(message); };
+
+var customerNames = ["Ryan", "Bill", "Jack"];  // person's name of the car i will be working on
+
+customerName = customerNames[1];
+
+var totalBill = []; // puts the total bill in one array 
+
+
+var projectTools = ["Drill", "Wire Cutter", "Multimeter"];  // different tools i need
+var findCarHead = false;  // true = I need to find a new head unit, false = I do not need a new head unit
+var carSpeaker = 3;  // number of car speakers needed
+var carSub = true;  // true = sub is needed, false = no sub needed
+var carSubType = false;  // true = wooden box, false = fiberglass box
+
+say("I now need to have " + customerName + " pick out a subwoofer from this list.");
+
+var productNames = ['8" sub', '10" sub', '12" sub', '15" sub'];
+var productCosts = [60, 80, 110, 140];
+
+for (var i = 0; i < productNames.length; i++) {
+	say( "The " + productNames[i] + " is $" + productCosts[i]);
+};
+
+productName = productNames[2];
+productCost = productCosts[2];
+totalBill.push(productCosts[2]);
+
+say("Since " + customerName + " picked out the " + productName + " from the list, $" + productCost + " will be added to his total.");
+
+say("His total is now " + );
+
+var screwsTotal = 8;
+say("Now I need to put in the screws.");
+while ( screwsTotal > 0 ) {
+	say(screwsTotal + " screws left to screw in.")
+	screwsTotal-=2;
+};
+say("Now all the screws are screwed in!");
+
+
+
+
+
+
+
+
+
+
+
+if (projectTools === true)  {
+	var toolsAnswer = ("and I have all the tools I need to get started!");
+} else {
+	var toolsAnswer = ("but I need to find my tools first.");
+}
+
+if (findCarHead === true)  {
+	var headAnswer = ("I'm going to have to find a new Head Unit ");
+} else {
+	var headAnswer = ("The Head Unit will work ");
+}
+
+if (carSpeaker > 0)  {
+	var speakerAnswer = ("and I need to purchase ", carSpeaker, " speakers for this car." );
+} else {
+	var speakerAnswer = ("and all the speakers are good!");
+}
+
+if (carSub === true)  {
+	var subAnswer = ("I will need to get a subwoofer ");
+	if (carSubType === true)  {
+	var boxAnswer = ("and make a wooden box for the Sub.");
+	} else {
+	var boxAnswer = ("and make a fiberglass box for the Sub.");
+	}
+} else {
+	var subAnswer = ("No subwoofer will be needed. :(");
+}
+
+console.log ("I am working on a project for ", customerName[0], ", ", toolsAnswer, headAnswer, speakerAnswer, subAnswer, boxAnswer);
 
 // Activity: Boolean Logic
 //var p = true, q = false, r = false;
