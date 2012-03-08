43bfe7ebbd3c3d9349d4ef4183abe1a4b5d3fc44
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-08 10:25:02 -0600

    starting to assembe the different components of the script together to clean it up.

--- westad_zach_assignment2.js
+++ westad_zach_assignment2.js
@@ -7,27 +7,46 @@
 // function to shorten 'console.log()' statements wit 'say()'.
 var say = function(message) { console.log(message); };
 
+
+
 // This if the function to list the Head Units and their cost.
 var pickOneHead = function() {
 	var headName = headNames[headNumber];
 	var costThisDeck = headCosts[headNumber];
 	say( "The " + headNames[headNumber] + " is $" + headCosts[headNumber]);
 };
+
 // This if the function to list the Subs and their cost.
 var pickOneSub = function() {
 	var subName = subNames[subNumber];
 	var costThisSub = subCosts[subNumber];
 	say( "The " + subNames[subNumber] + " is $" + subCosts[subNumber]);
 };
+
+// This function determines mow much money i need to collect or give change for.
+var buyAllParts = function(givenMoney) {
+	var projectCost = sum, 
+		wholeDollars;
+	if (givenMoney < projectCost) {
+		wholeDollars = sum - givenMoney;
+		{ return say("Hey! You still need to give me $" + wholeDollars); }
+	}
+
+	else {
+		(givenMoney > sum)
+		wholeDollars = givenMoney - sum
+		{return say("Thank you for giving me $" + givenMoney + ". Now here is $" + wholeDollars + " back as change.");
+	} }
+};
+
 var customerNames = ["Ryan", "Bill", "Jack"];  // person's name of the car i will be working on
 	customerName = customerNames[1];
 var totalBill = []; // puts the total bill in one array 
-var projectTools = ["Drill", "Wire Cutter", "Multimeter"];  // different tools i need
-var findCarHead = false;  // true = I need to find a new head unit, false = I do not need a new head unit
+var projectTools = true;  // true = I have all the tools,  false = i am missing some of my tools
+var findCarHead = true;  // true = I need to find a new head unit, false = I do not need a new head unit
 var carSpeaker = 3;  // number of car speakers needed
 var carSub = true;  // true = sub is needed, false = no sub needed
 var carSubType = false;  // true = wooden box, false = fiberglass box
-
 
 
 var headNames = ["Sony", "Kenwood", "Pioneer", "Pyle"];
@@ -38,11 +57,25 @@ say("First I need to have " + customerName + " pick out a Head Unit. Here are hi
 for (var headNumber = 0; headNumber < headNames.length; headNumber++) { 
 	pickOneHead();
 };
+// this area here you change the number in the three arrays to corespond with the correct ead unit
 headName = headNames[1];
 headCost = headCosts[1];
 totalBill.push(headCosts[1]);
 
 say(customerName + " picked out the " + headName + " Head Unit, so $" + headCost + " will be added to his total.");
+
+
+
+if (carSub === true)  {
+	var subAnswer = ("I will need to get a subwoofer ");
+	if (carSubType === true)  {
+	var boxAnswer = ("and make a wooden box for the Sub.");
+	} else {
+	var boxAnswer = ("and make a fiberglass box for the Sub.");
+	}
+} else {
+	var subAnswer = ("No subwoofer will be needed. :(");
+}
 
 
 say("I now need to have " + customerName + " pick out a subwoofer from this list.");
@@ -53,7 +86,7 @@ var subCosts = [60, 90, 110, 140];
 for (var subNumber = 0; subNumber < subNames.length; subNumber++) { 
 	pickOneSub();
 };
-
+// this area here you change the number in the three arrays to corespond with the correct ead unit
 subName = subNames[2];
 subCost = subCosts[2];
 totalBill.push(subCosts[2]);
@@ -68,7 +101,11 @@ say("Since " + customerName + " picked out the " + subName + " from the list, $"
 
 say("My next step is to put everything together.");
 
-
+if (projectTools === true)  {
+	var toolsAnswer = ("and I have all the tools I need to get started!");
+} else {
+	var toolsAnswer = ("but I need to find my tools first.");
+}
 
 
 var screwsTotal = 8;
@@ -89,15 +126,15 @@ var sum = 0;
 for (var i = 0; i < totalBill.length ; i++) {
          sum = sum + totalBill[i];
 };
-say("the total bill was " + sum);
+say("The total bill was " + sum);
+
+buyAllParts(300);
 
 
 
 
 
 
-
-/*
 if (projectTools === true)  {
 	var toolsAnswer = ("and I have all the tools I need to get started!");
 } else {
@@ -128,7 +165,7 @@ if (carSub === true)  {
 }
 
 console.log ("I am working on a project for ", customerName[0], ", ", toolsAnswer, headAnswer, speakerAnswer, subAnswer, boxAnswer);
-*/
+
 
 
 // Activity: Boolean Logic
