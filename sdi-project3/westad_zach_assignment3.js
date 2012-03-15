36503d09e7635c2fdd6f458800a899c156317c7b
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-15 10:58:13 -0500

    fixed the error in the 'for'loop that was causing the labor to not be added together.

--- sdi-project3/westad_zach_assignment3.js
+++ sdi-project3/westad_zach_assignment3.js
@@ -6,6 +6,8 @@
 
 // These variables are arrays that keep the total bill and hours together
 var cart = [];
+var timeUsed = [];
+
 
 // this references the json_assignment3.js file and you change the number in the array
 var customer = jsonCust.customers[3]
@@ -27,12 +29,25 @@ var projects = {
 }
 
 
+// this area adds the hours of everything to the projects.timeUsed array
+timeUsed.push(stereo.timeConsumed * headNumber);
+timeUsed.push(speaker.timeConsumed * speakerNumber);
+timeUsed.push(subwoofer.timeConsumed * subNumber);
+
 // this area adds the cost of everything to the cart array
 cart.push(stereo.price * headNumber);
 cart.push(speaker.price * speakerNumber);
 cart.push(subwoofer.price * subNumber);
 
+// adds the total hours of labor
+var laborSum = 0;
+for (var i = 0; i < timeUsed.length ; i++) {
+         laborSum = laborSum + timeUsed[i];
+};
 
+// calculates the labor costs
+var totalLaborCost = laborSum * 15;
+cart.push(totalLaborCost);
 
 // script to add all the units in the array tatalBill
 var sum = 0;
@@ -56,4 +71,6 @@ projects.say("Since I now know what i need to install, I can get the wiring diag
 projects.say("After all the components and the labor, the bill comes to $" + sum + ".");
 projects.say("I give " + customer.name + " the bill and they pay me for the job!");
 
-projects.say(cart)
+
+
+projects.say(laborSum);
