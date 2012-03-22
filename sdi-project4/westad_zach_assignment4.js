cfc4e7a95d97ca4bdd9cb9ec6399278f09562bc4
avguy133@fullsail.edu <avguy14@Zachs-MacBook-Pro.local>
2012-03-21 22:27:02 -0500

    finished the email verification coding and added to the project.

--- sdi-project4/westad_zach_assignment4.js
+++ sdi-project4/westad_zach_assignment4.js
@@ -47,7 +47,7 @@ var myPhoneString = "507-330-2374",
         lastNumber = true;   
         } else lastNumber = false;
     // take the values in the function and decides if the phone Number is valid
-        if (areaCode, midNumber, lastNumber === true) {
+        if (areaCode && midNumber && lastNumber === true) {
         myPhoneString = "valid";
         } else myPhoneString = "invalid";
         
@@ -58,3 +58,54 @@ var myPhoneString = "507-330-2374",
 checkNumber();
 // console log to display if the number is valid
 console.log("The phone number " + phoneNumberDisplay + " is " + myPhoneString);
+
+
+
+
+// Does a string follow a pattern like an email adress?.....
+
+
+// variables to change the email adress to be validated
+var emailAdress = "avguy133@fullsail.edu";
+var emailAdressDisplay = emailAdress;
+var checkEmail = function() {
+
+    
+    var startFirst = 0,
+        endFirst = emailAdress.indexOf("@"),
+        startMid = emailAdress.indexOf("@") + 1,
+        endMid = emailAdress.indexOf("."),
+        startLast = emailAdress.indexOf(".") + 1,
+        endLast = emailAdress.length;
+    
+    // this section takes the output from the last section and shortens the text and determines the # of digits in a section
+    var emailName = emailAdress.substring(startFirst,endFirst),
+        emailDomain = emailAdress.substring(startMid,endMid),
+        emailTLD = emailAdress.substring(startLast,endLast);
+        emailName = (endFirst),
+        emailDomain = (endMid - startMid),
+        emailTLD = (emailAdress.length - (emailAdress.indexOf(".")+1));
+
+   
+    if(emailName > 2) {
+        emailName = true;   
+    } else emailName = false;
+    if(emailDomain > 2) {
+        emailDomain = true;
+    } else emailDomain = false;
+    if(emailTLD < 5) {
+        emailTLD = true;   
+    } else emailTLD = false;
+    
+        if (emailName && emailDomain && emailTLD === true) {
+        result = "valid";
+        } else result = "invalid";
+
+    
+        
+    return result;
+};
+//runs the function
+checkEmail();
+// console log to display the outcome
+console.log("The email adress '" + emailAdressDisplay + "' is " + result);
